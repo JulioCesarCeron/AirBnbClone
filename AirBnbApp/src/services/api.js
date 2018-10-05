5444bb734c801ecd3644b2524e27b6c246f07d47
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-10-05 13:14:40 -0300

    added interceptors to all requests

--- AirBnbApp/src/services/api.js
+++ AirBnbApp/src/services/api.js
@@ -4,4 +4,18 @@ const api = axios.create({
 	baseURL: 'http://192.168.0.8:3333'
 });
 
+api.interceptors.request.use(async (config) => {
+	try {
+		const token = await AsyncStorage.getItem('@AirBnbApp:token');
+
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+
+		return config;
+	} catch (error) {
+		alert(error);
+	}
+});
+
 export default api;
